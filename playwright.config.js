10cb70b13493edc5d15220a4a1f3aec1b2fef99d
Arvinder Singh <arvinder06@gmail.com>
2022-05-04 22:11:36 +1000

    Screnshots and trace

--- playwright.config.js
+++ playwright.config.js
@@ -11,7 +11,9 @@ const config = {
   use: {
     // Test properties/metadata like browser,retries etc
     browserName: 'chromium',
-    headless: false
+    headless: false,
+    screenshot: 'retain-on-failure', //screenshot on every step in report
+    trace: 'retain-on-failure' // Log in report of every step and action performed for trace all tests give 'on'
   },
 };
 
