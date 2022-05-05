069261997410f156b8039f76ebe55885720b583e
Arvinder Singh <arvinder06@gmail.com>
2022-05-05 17:43:17 +1000

    Create order

--- tests/webApiTests.spec.js
+++ tests/webApiTests.spec.js
@@ -1,6 +1,6 @@
 const { test, expect, request } = require('@playwright/test')
-const loginCredentials = { userEmail: "anshika@gmail.com", userPassword: "Iamking@000" }
 let token;
+let orderID;
 // request library is used to Web API testing in playwright
 
 //execute before all the tests (not before each test)
@@ -9,13 +9,36 @@ test.beforeAll(async () => {
     const apiContext = await request.newContext();
     const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
         {
-            data: loginCredentials
+            data: {
+                userEmail: "arvinder06@gmail.com",
+                userPassword: "Abcde@12345"
+            }
         }
     )
     expect(loginResponse.ok()).toBeTruthy(); //playwright function to check response 200 and retun true
     const loginResponseJsonBody = await loginResponse.json();
     token = await loginResponseJsonBody.token;
     console.log(token);
+
+    //Create order API
+    const createOrderAPIResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
+        {
+            data: {
+                orders: [
+                    { country: "Australia", productOrderedId: "6262e95ae26b7e1a10e89bf0" }
+                ]
+            },
+            headers: {
+                'Authorization': token,
+                'Content-Type': 'application/json'
+            }
+        }
+    )
+    expect(createOrderAPIResponse.ok()).toBeTruthy();
+    const createOderJsonBody = await createOrderAPIResponse.json();
+    console.log(createOderJsonBody)
+    orderID = createOderJsonBody.orders[0];
+    console.log('Oder ID created: ' + orderID);
 })
 
 // this block of code will execute before each test
@@ -31,6 +54,6 @@ test.only('TC001_API Testing - Grab API response', async ({ page }) => {
     }, token); //here at run time, the token will go into the value and then the value will be stored as token via the javascript
 
     await page.goto('https://rahulshettyacademy.com/client');
-    await page.pause();
+    // await page.pause();
     await page.waitForLoadState('networkidle'); //This will wait for network layer to be stable
 })
