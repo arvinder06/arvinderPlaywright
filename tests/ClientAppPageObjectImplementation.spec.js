3d5a3992a128b91a72243754c684d0daae320c81
Arvinder Singh <arvinder06@gmail.com>
2022-05-10 11:31:29 +1000

    Parameterise test data with same test case

--- tests/ClientAppPageObjectImplementation.spec.js
+++ tests/ClientAppPageObjectImplementation.spec.js
@@ -2,40 +2,41 @@ const { test, expect } = require('@playwright/test');
 const { PageObjectManager } = require('../pageObjects/PageObjectManager');
 const dataSet = JSON.parse(JSON.stringify(require('../test-data/placeOrderTestData.json'))) // JSON FIle --> String --> JS Object to traverse easily
 
-//RSA = Rahul Shetty Academy
-test.only('TC001_ClientApp_Pages with Neetwrok call_RSA Shopping Website', async ({ page }) => {
-    const pageObectManager = new PageObjectManager(page);
+for (const data of dataSet) { //this is to run the same test for different set of data from json
+    test.only(`TC001_ClientApp_Pages with Neetwrok call_RSA Shopping Website for product ${data.productName}`, async ({ page }) => { // The test name should be unique else playwright will throw error
+        const pageObectManager = new PageObjectManager(page);
 
-    //Pages
-    const loginPage = pageObectManager.getLoginPage();
-    const dashboardPage = pageObectManager.getDashboardPage();
-    const checkoutPage = pageObectManager.getCheckoutPage();
-    const ordersReviewPage = pageObectManager.getOrdersReviewPage();
-    const ordersHistoryPage = pageObectManager.getOrdersHistoryPage();
+        //Pages
+        const loginPage = pageObectManager.getLoginPage();
+        const dashboardPage = pageObectManager.getDashboardPage();
+        const checkoutPage = pageObectManager.getCheckoutPage();
+        const ordersReviewPage = pageObectManager.getOrdersReviewPage();
+        const ordersHistoryPage = pageObectManager.getOrdersHistoryPage();
 
-    //Login Page
-    // const loginPage = new LoginPage(page);
+        //Login Page
+        // const loginPage = new LoginPage(page);
 
-    await loginPage.gotoURL('https://rahulshettyacademy.com/client')
-    await loginPage.loginViaUI(dataSet.userName, dataSet.password);
+        await loginPage.gotoURL('https://rahulshettyacademy.com/client')
+        await loginPage.loginViaUI(data.userName, data.password);
 
-    //Dashboard Page
-    // const dashboardPage = new DashboardPage(page);
-    await dashboardPage.addProductToCart(dataSet.productName);
-    await dashboardPage.navigateToCart();
+        //Dashboard Page
+        // const dashboardPage = new DashboardPage(page);
+        await dashboardPage.addProductToCart(data.productName);
+        await dashboardPage.navigateToCart();
 
-    //Checkout Page
-    // const checkoutPage = new CheckoutPage(page);
-    await checkoutPage.verifyProductInCart(dataSet.productName);
-    await checkoutPage.clickCheckout();
+        //Checkout Page
+        // const checkoutPage = new CheckoutPage(page);
+        await checkoutPage.verifyProductInCart(data.productName);
+        await checkoutPage.clickCheckout();
 
-    //Place order
-    await ordersReviewPage.searchCountryAndSelect(dataSet.countryCode, dataSet.countryName);
-    const orderId = await ordersReviewPage.SubmitAndGetOrderId();
-    console.log(orderId);
+        //Place order
+        await ordersReviewPage.searchCountryAndSelect(data.countryCode, data.countryName);
+        const orderId = await ordersReviewPage.SubmitAndGetOrderId();
+        console.log(orderId);
 
-    //Verify the order
-    await dashboardPage.navigateToOrders();
-    await ordersHistoryPage.searchOrderAndSelect(orderId);
-    expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
-})
+        //Verify the order
+        await dashboardPage.navigateToOrders();
+        await ordersHistoryPage.searchOrderAndSelect(orderId);
+        expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
+    })
+}
